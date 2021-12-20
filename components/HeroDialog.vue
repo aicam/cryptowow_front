8842dd47409e22a59b268167c95027b255f57e67
ali <a_icam@yahoo.com>
2021-12-20 17:19:13 +0330

    hero dialog started

--- components/HeroDialog.vue
+++ components/HeroDialog.vue
@@ -20,7 +20,10 @@
           </v-btn>
           <v-toolbar-title>Hero Full Inspection</v-toolbar-title>
         </v-toolbar>
-        <v-subheader class="text-center justify-center" style="font-size: 40px; font-weight: bold; font-family: 'DejaVu Math TeX Gyre',fantasy;">{{ heroName }}</v-subheader>
+        <v-subheader class="text-center justify-center"
+                     style="font-size: 40px; font-weight: bold; font-family: 'DejaVu Math TeX Gyre',fantasy;">
+          {{heroName}}
+        </v-subheader>
         <v-sheet
           v-if="loading"
           class="pa-3"
@@ -51,6 +54,12 @@
         <v-row v-if="!loading" class="flex" justify="start">
           <span class="bg" :style="{background: 'url(\'' + backgroundImage + '\')'}"></span>
           <v-col lg="3">
+            <v-row justify="center">
+              <v-img class="wow_icons my-10" max-width="55px"
+                     :src="heroClasses[heroClass].icon"/>
+              <v-img class="wow_icons my-10" max-width="55px"
+                     :src="heroRaces[race][gender == true ? 1 : 0]"/>
+            </v-row>
             <v-list flat>
               <v-list-item-group
                 v-model="selectedMenu"
@@ -69,6 +78,8 @@
           <v-col lg="9" class="justify-center">
             <v-stepper v-model="selectedMenu" non-linear style="background: transparent;">
               <v-stepper-items>
+
+                <!-- Equipments section -->
                 <v-stepper-content step="0">
                   <v-row>
                     <v-col lg="4">
@@ -77,16 +88,16 @@
                           <v-list-item v-for="(item, i) in leftItems"
                                        :key="i"
                                        style="padding: 0; margin-bottom: -26px;">
-                              <v-list-item-icon>
-                                <a href="#" :data-wowhead="`item=${item.id}`">
+                            <v-list-item-icon>
+                              <a href="#" :data-wowhead="`item=${item.id}`">
                                 <v-img style="border: 1px solid orange;" width="44px" :src="item.icon"/>
-                                </a>
-                              </v-list-item-icon>
-                              <v-list-item-content>
-                                <a href="#" :data-wowhead="`item=${item.id}`">
+                              </a>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                              <a href="#" :data-wowhead="`item=${item.id}`">
                                 <p style="font-size: 12px; color: white;">{{item.name}}</p>
-                                </a>
-                              </v-list-item-content>
+                              </a>
+                            </v-list-item-content>
                           </v-list-item>
                         </v-list-item-group>
                       </v-list>
@@ -137,6 +148,37 @@
                     </v-col>
                   </v-row>
                 </v-stepper-content>
+
+                <!-- Achievements Section -->
+                <v-stepper-content step="1">
+                  <v-card>
+                    <v-sheet
+                      v-if="achievementLoading"
+                      class="pa-3"
+                    >
+                      <v-skeleton-loader
+                        class="mx-auto"
+                        type="card"
+                      ></v-skeleton-loader>
+                    </v-sheet>
+                    <v-virtual-scroll v-if="!achievementLoading"
+                                      :items="achievements"
+                                      height="300"
+                                      item-height="100"
+                    >
+                      <template v-slot:default="{item}">
+                        <v-list-item :key="item.id">
+                          <v-list-item-content>
+                            <v-list-item-title>
+                              {{item.name}}
+                            </v-list-item-title>
+                            {{item.description}}
+                          </v-list-item-content>
+                        </v-list-item>
+                      </template>
+                    </v-virtual-scroll>
+                  </v-card>
+                </v-stepper-content>
               </v-stepper-items>
             </v-stepper>
           </v-col>
@@ -147,6 +189,8 @@
 </template>
 
 <script>
+    import {wowDicts} from '../components/wowDicts'
+
     export default {
         middleware: 'auth',
         name: "HeroDialog",
@@ -167,8 +211,9 @@
                         this.race = respData.race;
                         this.gender = respData.gender;
                         this.level = respData.level;
-                        this.class = respData.class;
+                        this.heroClass = respData.class;
                         this.equippedCache = respData.equipment_cache;
+                        this.achievements = respData.achievements;
                         const eqsplitted = this.equippedCache.split(" ");
                         for (let i = 0; i < eqsplitted.length; i += 2) {
                             this.items.push(eqsplitted[i])
@@ -177,12 +222,20 @@
                             this.backgroundImage = require("~/static/hero_dialogs/orgrimmar_blur.png");
                         else
                             this.backgroundImage = require("~/static/hero_dialogs/stormwind_blur.png");
+
+                        // parse items
                         const parseItemId = async (itemID) => {
                             var requestOptions = {
                                 method: 'GET',
                                 redirect: 'follow',
                             };
                             let iteminfo = {};
+                            if (itemID == "0") {
+                                iteminfo.icon = "https://wow.zamimg.com/images/wow/icons/large/inv_misc_questionmark.jpg"
+                                iteminfo.name = "No item";
+                                iteminfo.id = "0";
+                                iteminfo.level = "0";
+                            }
                             const storageInfo = localStorage.getItem(itemID);
                             // Cache system
                             if (storageInfo !== null) {
@@ -207,6 +260,8 @@
                                 .catch(error => console.log('error', error));
                             return iteminfo
                         };
+
+
                         for (let i = 0; i < 8; i++) {
                             let r1 = await parseItemId(this.items[i]);
                             this.leftItems.push(r1);
@@ -220,7 +275,8 @@
                                 this.bottomItems.push(r3)
                             }
                         }
-                        console.log(this.leftItems[0].icon);
+                        console.log(this.heroClass);
+                        console.log(this.heroClasses);
                         this.loading = false;
                     })
                 } else {
@@ -233,6 +289,34 @@
         methods: {
             closeDialog() {
                 this.$emit("close-func")
+            },
+            async changeMenu(step) {
+                this.selectedMenu = step;
+                if (step == 1) {
+                    this.achievementLoading = true;
+                    // Parse achievements
+                    const parseAchievementID = async (aID) => {
+                        var requestOptions = {
+                            method: 'GET',
+                            redirect: 'follow',
+                        };
+                        let aInfo = {};
+                        await fetch("http://localhost:44297/https://www.wowhead.com/achievement=" + aID, requestOptions)
+                            .then(response => response.text())
+                            .then(result => {
+                                const rigidInfo = JSON.parse(result.split('<script type="application/ld+json">')[1].split("<\/script>")[0]);
+                                aInfo.id = aID;
+                                aInfo.name = rigidInfo.name;
+                                aInfo.description = rigidInfo.description;
+                                console.log(rigidInfo);
+                            });
+                        return aInfo
+                    };
+                    for (let i = 0; i < this.achievements.length; i++) {
+                        this.achievements[i] = await parseAchievementID(this.achievements[i])
+                    }
+                    this.achievementLoading = false;
+                }
             }
         },
         data() {
@@ -241,12 +325,13 @@
                 race: 0,
                 gender: false,
                 level: 0,
-                class: 0,
+                heroClass: 0,
                 equippedCache: "",
                 items: [],
                 leftItems: [],
                 rightItems: [],
                 bottomItems: [],
+                achievements: [],
                 selectedMenu: 0,
                 menuItems: ["Equipments", "Achievements", "Reputation", "Mounts", "Pets"],
                 backgroundImage: "",
@@ -254,6 +339,9 @@
                 notifications: false,
                 sound: true,
                 widgets: false,
+                heroClasses: wowDicts.heroClasses,
+                heroRaces: wowDicts.heroRaces,
+                achievementLoading: true,
             }
         },
     }
@@ -269,5 +357,13 @@
     /*background: url("~/static/hero_dialogs/stormwind_blur.png");*/
     background-size: cover;
     transform: scale(1);
+  }
+
+  .wow_icons {
+    border: 1px solid darkmagenta;
+  }
+
+  .v-image__image--contain {
+    background-position: left center !important;
   }
 </style>
